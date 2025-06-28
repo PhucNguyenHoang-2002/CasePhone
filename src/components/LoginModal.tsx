d7aa7ebf7ef99ba06ca055dd499e5c6ba634de03
PhucNguyenHoang-2002 <116200803+PhucNguyenHoang-2002@users.noreply.github.com>
2025-06-28 16:52:24 +0700

    Update LoginModal.tsx

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -23,8 +23,8 @@ const LoginModal = ({
         <DialogHeader>
           <div className='relative mx-auto w-24 h-24 mb-2'>
             <Image
-              src='/snake-1.png'
-              alt='snake image'
+              src='/Tent-3.png'
+              alt='tent image'
               className='object-contain'
               fill
             />
